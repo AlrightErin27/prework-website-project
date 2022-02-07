36af01717e7e35e4d9458171a944b1a0160a5b40
Erin Van Brunt <erinmontybruce@gmail.com>
2022-02-06 19:30:57 -0800

    all photos are now 13em tall & centered

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const button = document.querySelector(`#button`);
 
 button.addEventListener(`click`, function (event) {
-  //   console.log(`Button Clicked`);
   //generate random number between 0-10
   const randomNum = Math.floor(Math.random() * 11);
-  console.log(randomNum);
+  //   console.log(randomNum);
 
+  //inserts random number into path to get fish image
   const source = `fishes/fish${randomNum}.png`;
-  //   const fishContainer = document.querySelector(`#fish-container`);
   const image = new Image();
-
   image.src = source;
+  //all photos are now 13em tall & centered
+  image.style.height = "13em";
   document.querySelector(`#fish-container`).appendChild(image);
 });
