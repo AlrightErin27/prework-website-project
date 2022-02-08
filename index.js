4665f0952b8efab20022a7d52583bae5c0ff19b8
Erin Van Brunt <erinmontybruce@gmail.com>
2022-02-07 16:05:48 -0800

    new fish image appears when button clicked. old image goes away

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 const button = document.querySelector(`#button`);
+let count = 0;
 
 button.addEventListener(`click`, function (event) {
-  //generate random number between 0-10
-  const randomNum = Math.floor(Math.random() * 11);
-  //   console.log(randomNum);
-
   //inserts random number into path to get fish image
-  const source = `fishes/fish${randomNum}.png`;
+  //generate random number between 0-10
+  const source = `fishes/fish${Math.floor(Math.random() * 11)}.png`;
   const image = new Image();
   image.src = source;
   //all photos are now 13em tall & centered
   image.style.height = "13em";
-  document.querySelector(`#fish-container`).appendChild(image);
+
+  // console.log(document.querySelector(`img`));
+
+  //if there is already a generated image, remove it and replace it with random fish.
+  //If not, just add random fish image.
+  if (document.querySelector(`img`)) {
+    document.querySelector(`img`).remove();
+    document.querySelector(`#fish-container`).appendChild(image);
+  } else {
+    document.querySelector(`#fish-container`).appendChild(image);
+  }
 });
